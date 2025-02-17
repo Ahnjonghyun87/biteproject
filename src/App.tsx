22cd9210b01772793f8cf475ebf3831cd52128d2
[Github <helgo1318@gmail.com>
2025-02-18 00:47:09 +0900

    프로젝트 기본 라이브러리 설치. 프리티어 설정, reset css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import MainBackGround from "./components/MainBackGround";
 import SideBar from "./components/SideBar";
-
+import "./reset.css";
 function App() {
   return (
     <section className="App">
