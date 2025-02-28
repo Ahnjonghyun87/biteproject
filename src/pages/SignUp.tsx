908bac94717501ac50b41e368c7fd927f45f8fad
[Github <helgo1318@gmail.com>
2025-02-28 23:58:20 +0900

    현재 mui와 리액트 훅폼으로 로그인창 만드는중. form  수정필요

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,10 +1,19 @@
-import { Box } from "@mui/material";
+import { Button, Container, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import CommonModal from "../components/common/modal/CommonModal";
 
 interface LoginModalStatus {
   setIsSignUpOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
+
+interface SignUpData {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  nickname: string;
+}
+
 const SignUp: React.FC<LoginModalStatus> = ({ setIsSignUpOpen }) => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -15,6 +24,17 @@ const SignUp: React.FC<LoginModalStatus> = ({ setIsSignUpOpen }) => {
     setIsSignUpOpen(false);
   };
 
+  const { register, handleSubmit, formState, watch, control } = useForm<SignUpData>({
+    mode: "onChange",
+    defaultValues: {
+      email: "",
+      nickname: "",
+      password: "",
+    },
+  });
+
+  const password = watch("password");
+
   return (
     <div>
       {isOpen && (
@@ -24,7 +44,7 @@ const SignUp: React.FC<LoginModalStatus> = ({ setIsSignUpOpen }) => {
           size="large"
           onClose={closeModal}
           content={
-            <Box
+            <Container
               sx={{
                 maxWidth: "800px", // 컨테이너 최대 너비 지정
                 padding: "20px", // 내부 여백 추가
@@ -33,18 +53,93 @@ const SignUp: React.FC<LoginModalStatus> = ({ setIsSignUpOpen }) => {
                 letterSpacing: "0.05em", // 글자 간격 조정
               }}
             >
-              Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical
-              Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at
-              Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a
-              Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the
-              undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum"
-              (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of
-              ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
-              amet..", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is
-              reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum"
-              by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914
-              translation by H. Rackham.
-            </Box>
+              <form onSubmit={handleSubmit(onsubmit)}>
+                <Controller
+                  name="email"
+                  control={control}
+                  defaultValue=""
+                  rules={{
+                    required: "이메일을 입력해주세요",
+                    pattern: {
+                      value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                      message: "유효하지 않은 이메일 형식입니다",
+                    },
+                  }}
+                  render={({ field: { onChange, value }, fieldState: { error } }) => (
+                    <TextField
+                      id="userEmail"
+                      placeholder="E-mail"
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{ maxLength: 50 }}
+                      value={value}
+                      onChange={onChange}
+                      error={!!error}
+                      helperText={error ? error.message : `${value.length}/50`}
+                    />
+                  )}
+                />
+                <Controller
+                  name="password"
+                  control={control}
+                  defaultValue=""
+                  rules={{
+                    required: "비밀번호를 입력해주세요",
+                    pattern: {
+                      value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$/,
+                      message: "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다 (8~20자)",
+                    },
+                  }}
+                  render={({ field: { onChange, value }, fieldState: { error } }) => (
+                    <TextField
+                      id="password"
+                      placeholder="password"
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{ maxLength: 20 }}
+                      value={value}
+                      onChange={onChange}
+                      error={!!error}
+                      helperText={error ? error.message : `${value.length}/20`}
+                    />
+                  )}
+                />
+                <Controller
+                  name="nickname"
+                  control={control}
+                  defaultValue=""
+                  rules={{
+                    required: "닉네임을 입력해주세요",
+                    pattern: {
+                      value: /^[가-힣A-Za-z\d]{2,15}$/,
+                      message: "닉네임은 영문,한글과 숫자로 3~15자 이내로 입력해주세요",
+                    },
+                  }}
+                  render={({ field: { onChange, value }, fieldState: { error } }) => (
+                    <TextField
+                      id="userNickname"
+                      placeholder="nickname"
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{ maxLength: 15 }}
+                      value={value}
+                      onChange={onChange}
+                      error={!!error}
+                      helperText={error ? error.message : `${value.length}/15`}
+                    />
+                  )}
+                />
+                <Button variant="contained" type="submit">
+                  가입
+                </Button>
+              </form>
+            </Container>
           }
         />
       )}
