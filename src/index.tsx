4f97725717dd799cbd0a1f6daa48a998a60f6936
[Github <helgo1318@gmail.com>
2025-03-06 22:44:06 +0900

    index 파일에 auth 프로바이더 지정.

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "react-oidc-context";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 const queryClient = new QueryClient();
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_BhGWshYc8",
+  client_id: "7kmrd1gjl6cotomrb15h6rb4s",
+  redirect_uri: "https://naver.com",
+  response_type: "code",
+  scope: "phone openid email",
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
+        <AuthProvider {...cognitoAuthConfig}>
+          <App />
+        </AuthProvider>
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
