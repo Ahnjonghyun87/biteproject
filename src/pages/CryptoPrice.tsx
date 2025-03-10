82f8ee8925ce3d6455595e0abcc890bd09ec60c6
[Github <helgo1318@gmail.com>
2025-03-10 23:46:02 +0900

    가격정보 관련 꾸미는중

--- src/pages/CryptoPrice.tsx
+++ src/pages/CryptoPrice.tsx
@@ -1,15 +1,16 @@
+import { Box, Button, Container, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import UpbitCoinPrice from "../types/upbitCoinPrice";
 const CryptoPrice = () => {
-  const [btcPrice, setBtcPrice] = useState<UpbitCoinPrice>();
+  const [btcEthPrice, setBtcEthPrice] = useState<UpbitCoinPrice>();
   // const QueryClient = useQuery<>({
   //   queryKey:["coinPrice", ]
   // })
   const GetPrice = async () => {
     try {
       const response = await axios.get("https://7o712sia8j.execute-api.ap-northeast-1.amazonaws.com/test1/items");
-      setBtcPrice(response.data);
+      setBtcEthPrice(response.data);
     } catch (error) {
       console.log(error);
     }
@@ -18,25 +19,43 @@ const CryptoPrice = () => {
   useEffect(() => {
     GetPrice();
   }, []);
+
+  const onClickPriceButton = () => {};
+
+  // const image = btcEthPrice?.market?KRW-BTC ?
   return (
-    <div>
-      CryptoPrice
-      <div>
-        {btcPrice ? (
-          btcPrice.map((b, i) => {
+    <Container sx={{ height: "75vh" }}>
+      <Box>
+        {btcEthPrice ? (
+          btcEthPrice.map((crypto, image) => {
             return (
-              <li key={b.market}>
-                <img key={i} src={`./images/${b.market.substring(4)}`}></img>
-                <span>{b.market.substring(4)}</span>
-                <span>{b.trade_price}</span>
-              </li>
+              <Box
+                display={"inline"}
+                justifyContent={"center"}
+                padding={5}
+                gap={4}
+                sx={{ fontSize: 48, width: "100%" }}
+                key={crypto.market}
+              >
+                <img
+                  height={100}
+                  width={100}
+                  key={crypto.market}
+                  src={crypto.market === "KRW-BTC" ? "/images/BTC.svg" : "/images/ETH.svg"}
+                ></img>
+                <Button variant="contained">{crypto.market.substring(4)}</Button>
+                <Typography component={"span"} padding={2} sx={{ fontSize: 36 }}>
+                  {crypto.trade_price}
+                </Typography>
+                {/* <Typography>{crypto.change}</Typography> */}
+              </Box>
             );
           })
         ) : (
           <div>로딩...</div>
         )}
-      </div>
-    </div>
+      </Box>
+    </Container>
   );
 };
 
